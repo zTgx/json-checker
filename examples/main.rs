185ac5607be5414ce093dace487c22d8f76d5c1a
zTgx <747674262@qq.com>
2019-08-22 00:51:26 +0800

    add examples

--- examples/main.rs
+++ examples/main.rs
@@ -1,8 +1,36 @@
 extern crate json_checker;
 use json_checker::*;
 
+extern crate ncurses;
+use ncurses::*;
+
 fn main() {
     let mut checker = JsonChecker::new(20);
 
-    
+    initscr();
+    raw();
+
+    keypad(stdscr(), true);
+    // noecho();
+
+    printw("Enter a json string: ");
+
+    loop {
+        let next_char = getch();
+        if next_char == 0xa {
+            endwin();
+            break;
+        }
+
+        if checker.check_char(next_char) == 0 {
+            endwin();
+            panic!("JSON_checker_end: syntax error\n");
+        }
+    }
+
+    if checker.done() == 0 {
+        panic!("JSON_checker_end: syntax error\n");
+    } else {
+        println!("well-formed JSON text!")
+    }
 }
